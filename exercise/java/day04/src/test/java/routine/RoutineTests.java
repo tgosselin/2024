cf081d2201f61f03f10e5acd6ecfd753f00ecc21
Thomas GOSSELIN <tgosselin@murex.com>
2024-12-04 15:25:31 +0000

    add test with own Test doubles

--- exercise/java/day04/src/test/java/routine/RoutineTests.java
+++ exercise/java/day04/src/test/java/routine/RoutineTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RoutineTests {
     @Test
     void startRoutineWithMockito() {
@@ -27,6 +29,83 @@ class RoutineTests {
     @Test
     void startRoutineWithManualTestDoubles() {
         // Write a test using your own test double(s)
-        
+        ReindeerFeederTest reindeerFeeder = new ReindeerFeederTest();
+        EmailServiceTest emailService = new EmailServiceTest(reindeerFeeder);
+        ScheduleServiceTest scheduleService = new ScheduleServiceTest(emailService);
+
+        Routine routine = new Routine(emailService, scheduleService, reindeerFeeder);
+        routine.start();
+
+        assertThat(scheduleService.isRoutineDone()).isTrue();
+    }
+
+    private static class EmailServiceTest implements EmailService {
+        private final ReindeerFeederTest reindeerFeeder;
+        private boolean emailsRead = false;
+
+        public EmailServiceTest(ReindeerFeederTest reindeerFeeder) {
+            this.reindeerFeeder = reindeerFeeder;
+        }
+
+        @Override
+        public void readNewEmails() {
+            if (!reindeerFeeder.hasBeenFed()) {
+                throw new IllegalArgumentException("Reindeer feeder has not been fed before reading emails");
+            }
+            emailsRead = true;
+        }
+
+        public boolean hasReadEmails() {
+            return emailsRead;
+        }
+    }
+
+    private static class ScheduleServiceTest implements ScheduleService {
+
+        private final EmailServiceTest emailService;
+        private Schedule todaySchedule = null;
+        private boolean routineDone = false;
+
+        public ScheduleServiceTest(EmailServiceTest emailService) {
+            this.emailService = emailService;
+        }
+
+        @Override
+        public Schedule todaySchedule() {
+            todaySchedule = new Schedule();
+            return todaySchedule;
+        }
+
+        @Override
+        public void organizeMyDay(Schedule schedule) {
+            if (todaySchedule == null || todaySchedule != schedule) {
+                throw new IllegalArgumentException("Schedule must be retrieved before organizing the day");
+            }
+        }
+
+        @Override
+        public void continueDay() {
+            if (!emailService.hasReadEmails()) {
+                throw new IllegalArgumentException("New emails have not been read before continuing the day");
+            }
+            routineDone = true;
+        }
+
+        public boolean isRoutineDone() {
+            return routineDone;
+        }
+    }
+
+    private static class ReindeerFeederTest implements ReindeerFeeder {
+        private boolean hasBeenFed = false;
+
+        @Override
+        public void feedReindeers() {
+            hasBeenFed = true;
+        }
+
+        public boolean hasBeenFed() {
+            return hasBeenFed;
+        }
     }
 }
