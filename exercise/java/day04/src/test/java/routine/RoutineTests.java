3f6b082562eafb6a29fe1c64e7959677f2026500
Thomas GOSSELIN <tgosselin@murex.com>
2024-12-04 15:02:28 +0000

    add tests with Mockito

--- exercise/java/day04/src/test/java/routine/RoutineTests.java
+++ exercise/java/day04/src/test/java/routine/RoutineTests.java
@@ -1,15 +1,32 @@
 package routine;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 
 class RoutineTests {
     @Test
     void startRoutineWithMockito() {
         // Write a test using Mockito
+        EmailService emailService = Mockito.mock(EmailService.class);
+        ScheduleService scheduleService = Mockito.mock(ScheduleService.class);
+        Schedule schedule = new Schedule();
+        Mockito.when(scheduleService.todaySchedule()).thenReturn(schedule);
+        ReindeerFeeder reindeerFeeder = Mockito.mock(ReindeerFeeder.class);
+
+        Routine routine = new Routine(emailService, scheduleService, reindeerFeeder);
+        routine.start();
+
+        InOrder inOrder = Mockito.inOrder(scheduleService, reindeerFeeder, emailService);
+        inOrder.verify(scheduleService).organizeMyDay(schedule);
+        inOrder.verify(reindeerFeeder).feedReindeers();
+        inOrder.verify(emailService).readNewEmails();
+        inOrder.verify(scheduleService).continueDay();
     }
 
     @Test
     void startRoutineWithManualTestDoubles() {
         // Write a test using your own test double(s)
+        
     }
 }
